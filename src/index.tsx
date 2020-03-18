d6f4eb22810e6c37d16c861fc04950b3a138782a
GewoonRene <rhuibertsjr@gmail.com>
2020-03-18 17:24:16 +0100

    Added: Center Scene

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,14 @@
 
 import React, { Fragment } from "react";
 import {render} from "react-dom";
+import s from './center.module.less';
 import './index.less';
 
 import SceneManager from "@containers/scene-manager/SceneManager";
 import Sprite from "@components/sprite/Sprite";
 
-import HeaderColor from './assets/Rectangle.svg';
-import FirstPhone from './assets/Scherm2.svg';
-import SecondPhone from './assets/Scherm3.svg';
-import RobinAssistant from  './assets/Robin.svg';
+import Petra from './assets/svg/petra.svg';
+import Bois from './assets/svg/petraandbois.svg';
 
 /*
  * App Component
@@ -17,10 +16,41 @@ import RobinAssistant from  './assets/Robin.svg';
 export const App = (): JSX.Element => (
   <Fragment>
     <SceneManager>
-      <Sprite classname="first" path={HeaderColor}/>
-      <Sprite classname="robin" path={RobinAssistant}/>
-      <Sprite classname="firstPhone" path={FirstPhone}/>
-      <Sprite classname="secondPhone" path={SecondPhone}/>
+
+      <div className={s.storyContainer}>
+
+        <div className={s.story}>
+          <div className={s.storyText}>
+            <h1>BEGELEIDEN VAN A.S.S</h1>
+            <p>Dit is Petra, zij is actief als begeleider met mensen die A.S.S hebben. Mensen begeleiden is haar passie. Al de activiteiten die ze met haar cliënten doet schrijft zij op om alles bij te houden. Soms is het lastig bij te houden door al het papierwerk.</p>
+          </div>
+          <div className={s.firstSprite}>
+            <Sprite path={Petra}/>
+          </div>
+        </div>
+
+        <div className={s.story}>
+          <div className={s.storyText}>
+            <h1>BEGELEIDING IS PER CLIENT VERSCHILLEND</h1>
+            <p>Elke client heeft een andere dag indeling. Door middel van een planning houdt Petra bij wat ze met een client samen doet. De ene client heeft meer begeleiding nodig dan de andere.</p>
+          </div>
+          <div className={s.secondSprite}>
+            <Sprite path={Bois}/>
+          </div>
+        </div>
+
+        <div className={s.story}>
+          <div className={s.storyText}>
+            <h1>ROBIN ASSISTANT</h1>
+            <p>Al dat papierwerk werd Petra te veel. De mappen stapelden op. Op dit moment gebruikt zij Robin Assistant om de taken per client bij te houden. Ze houdt nu ook per client meer tijd over en alles is makkelijk in een schema bij te houden via de telefoon of computer.</p>
+          </div>
+          <div className={s.firstSprite}>
+            <Sprite path={Petra}/>
+          </div>
+        </div>
+
+      </div>
+
     </SceneManager>
   </Fragment>
 );
