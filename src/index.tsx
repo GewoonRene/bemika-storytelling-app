005d3f15e8f03ee4d2a7be781aef32f5e59ac730
mishamatisse <maessmit@gmail.com>
2020-03-18 18:21:59 +0100

    Assets/ Header

--- src/index.tsx
+++ src/index.tsx
@@ -6,21 +6,48 @@ import './index.less';
 import SceneManager from "@containers/scene-manager/SceneManager";
 import Sprite from "@components/sprite/Sprite";
 
-import HeaderColor from './assets/Rectangle.svg';
+import HeaderColor from './assets/header-color.svg';
 import FirstPhone from './assets/Scherm2.svg';
-import SecondPhone from './assets/Scherm3.svg';
+import SecondPhone from './assets/schermagenda.svg';
 import RobinAssistant from  './assets/Robin.svg';
+import PointerDown from './assets/pointerdown.svg';
+import Arrow from './assets/arrow.svg';
 
 /*
  * App Component
  * * * */
+export const Daily = (): JSX.Element => (
+    <div className="quote">
+        YOUR DAILY ASSISTANT
+    </div>
+);
+
+export const Button = (): JSX.Element => (
+  <div className="button">
+          <a href="#">IK MELD ME AAN</a>
+  </div>
+);
+
+export const Scroll = (): JSX.Element => (
+  <div className="scrollDown">
+      SCROLL OM TE BEGINNEN
+  </div>
+);
+
 export const App = (): JSX.Element => (
   <Fragment>
     <SceneManager>
+      {/*  HEADER*/}
       <Sprite classname="first" path={HeaderColor}/>
       <Sprite classname="robin" path={RobinAssistant}/>
+      <Daily />
+      <Button />
       <Sprite classname="firstPhone" path={FirstPhone}/>
       <Sprite classname="secondPhone" path={SecondPhone}/>
+      <Scroll />
+      <Sprite classname="outerPointer" path={PointerDown} />
+      <Sprite classname="innerPointer" path={Arrow} />
+    {/*  END HEADER*/}
     </SceneManager>
   </Fragment>
 );
